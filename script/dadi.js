2505237315a946f594410043a14b0349847801ff
DanielFerraiuolo98 <daniel.ferraiuolo.98@gmail.com>
2024-11-04 15:09:55 +0100

    esercizi mail, dadi, snack 5 e snack 6 completati

--- script/dadi.js
+++ script/dadi.js
@@ -1,7 +1,7 @@
 /*Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
-/*
+
 let numeroUser = Math.floor((Math.random() * 6) + 1);
 let numeroPc = Math.floor((Math.random() * 6) + 1);
 console.log(numeroUser);
@@ -15,4 +15,4 @@ else if(numeroPc > numeroUser){
 }
 else{
     console.log("i numeri sono uguali");
-}*/
+}
